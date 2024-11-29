9ebd65b1441b043648c211f9cc5156251a734b08
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2024-11-29 13:08:46 +0200

    add multi condition

--- src/MultiCondition.js
+++ src/MultiCondition.js
@@ -1,3 +1,4 @@
+// source: https://stackoverflow.com/questions/71298163/react-final-form-double-conditional-form
 import { useFormState } from 'react-final-form';
 
 const MultiCondition = ({ condition, children }) => {
